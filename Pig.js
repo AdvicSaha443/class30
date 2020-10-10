c21104668f9211b475e561a44e235e235c218936
amazingadvicsaha443@gmail.com <amazingadvicsaha443@gmail.com>
2020-10-10 14:29:56 +0530

    vanishing pigs

--- Pig.js
+++ Pig.js
@@ -2,6 +2,21 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.visibility = 255;
+  }
+  display() {
+    if (this.body.speed < 3) {
+      super.display();
+    }
+    else {
+      var pos = this.body.position;
+      World.remove(world, this.body);
+      push();
+      this.visibility = this.visibility - 5;
+      tint(255, this.visibility);
+      image(this.image, pos.x, pos.y, 50, 50);
+      pop();
+    }
   }
 
 };
